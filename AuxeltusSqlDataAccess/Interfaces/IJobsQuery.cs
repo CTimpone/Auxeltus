0a326cfb80125b70d78400e621af1386a3f81c0a
CTimpone <ctimponegit@gmail.com>
2022-05-24 14:55:40 -0400

    Implement RetrieveOpenJobs

--- AuxeltusSqlDataAccess/Interfaces/IJobsQuery.cs
+++ AuxeltusSqlDataAccess/Interfaces/IJobsQuery.cs
@@ -5,7 +5,8 @@ namespace Auxeltus.AccessLayer.Sql
 {
     public interface IJobsQuery
     {
-        public async Task<Job> RetrieveJob(int jobId);
-        public async Task<List<Job>> RetrieveJobsReportingTo(int employeeId);
+        public Task<Job> RetrieveJob(int jobId);
+        public Task<List<Job>> RetrieveJobsReportingTo(int employeeId);
+        public Task<List<Job>> RetrieveOpenJobs(int? maxReturn, int? startIndex);
     }
 }
