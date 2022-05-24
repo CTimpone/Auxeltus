4872582886a3c6049d7319742225e6d18284aba0
CTimpone <ctimponegit@gmail.com>
2022-05-24 16:11:31 -0400

    Implement JobsCommand

--- AuxeltusSqlDataAccess/Commands/JobsCommand.cs
+++ AuxeltusSqlDataAccess/Commands/JobsCommand.cs
@@ -1,20 +1,77 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Auxeltus.AccessLayer.Sql
 {
     public class JobsCommand : IJobsCommand
     {
-        public void CreateJob(Job job)
+        private readonly ILogger _logger;
+        private readonly AuxeltusSqlContext _context;
+
+        public JobsCommand(ILogger logger, AuxeltusSqlContext context)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+            _context = context;
         }
-        public void UpdateJob(int jobId, Job job)
+
+        public async Task CreateJobAsync(Job job)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.Jobs
+                    .AddAsync(job)
+                    .ConfigureAwait(false);
+
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception thrown in {nameof(JobsCommand)}.{nameof(CreateJobAsync)}");
+                throw;
+            }
         }
-        public void DeleteJob(int jobId)
+
+        public async Task UpdateJobAsync(int jobId, Job updatedJob)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Job foundJob = await _context.Jobs
+                    .FirstAsync(job => job.Id == jobId)
+                    .ConfigureAwait(false);
+
+                foundJob.Mutate(updatedJob);
+
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception thrown in {nameof(JobsCommand)}.{nameof(UpdateJobAsync)}");
+                throw;
+            }
+        }
+
+        public async Task DeleteJobAsync(int jobId)
+        {
+            try
+            {
+                Job foundJob = await _context.Jobs
+                    .FirstAsync(job => job.Id == jobId)
+                    .ConfigureAwait(false);
+
+                _context.Remove(foundJob);
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception thrown in {nameof(JobsCommand)}.{nameof(DeleteJobAsync)}");
+                throw;
+            }
         }
     }
 }
