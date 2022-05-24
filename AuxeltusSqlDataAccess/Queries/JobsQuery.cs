792a0debaa61389d1893988890736fb8821edb7e
CTimpone <ctimponegit@gmail.com>
2022-05-24 11:48:22 -0400

    Recompose Jobs access interfaces with stubbed classes

---
+++ AuxeltusSqlDataAccess/Queries/JobsQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Auxeltus.AccessLayer.Sql
+{
+    public class JobsQuery : IJobsQuery
+    {
+        public Job RetrieveJob(int jobId)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Job> RetrieveJobsReportingTo(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
