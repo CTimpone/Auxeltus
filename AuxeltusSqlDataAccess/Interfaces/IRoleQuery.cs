ee1d55e9a01c3f5b56887001dd43e238fe6560e3
CTimpone <ctimponegit@gmail.com>
2022-05-24 16:41:08 -0400

    Implement RoleQuery

---
+++ AuxeltusSqlDataAccess/Interfaces/IRoleQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Auxeltus.AccessLayer.Sql
+{
+    public interface IRoleQuery
+    {
+        public Task<List<Role>> RetrieveRolesAsync(int? maxReturn, int? startIndex);
+    }
+}
