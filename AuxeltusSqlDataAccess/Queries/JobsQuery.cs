837fd470f8007e09a42ea0208dd3103098b7b185
CTimpone <ctimponegit@gmail.com>
2022-05-24 14:45:21 -0400

    Implement RetrieveJob

--- AuxeltusSqlDataAccess/Queries/JobsQuery.cs
+++ AuxeltusSqlDataAccess/Queries/JobsQuery.cs
@@ -1,15 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Auxeltus.AccessLayer.Sql
 {
     public class JobsQuery : IJobsQuery
     {
-        public Job RetrieveJob(int jobId)
+        private readonly ILogger _logger;
+        private readonly AuxeltusSqlContext _context;
+
+        public JobsQuery(ILogger logger, AuxeltusSqlContext context)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+            _context = context;
         }
-        public List<Job> RetrieveJobsReportingTo(int employeeId)
+
+        public async Task<Job> RetrieveJob(int jobId)
+        {
+            try
+            {
+                Job foundJob = await _context.Jobs
+                    .AsNoTracking()
+                    .FirstAsync(job => job.Id == jobId)
+                    .ConfigureAwait(false);
+
+                return foundJob;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception thrown in {nameof(JobsQuery)}.{nameof(RetrieveJob)}");
+                throw;
+            }
+        }
+
+        public async Task<List<Job>> RetrieveJobsReportingTo(int employeeId)
         {
             throw new NotImplementedException();
         }
